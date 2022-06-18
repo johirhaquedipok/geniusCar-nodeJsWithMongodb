83f8bb3b7fa9258e0ad977169c615282b6f83344
johirhaquedipok <johirhaquedipok2@gmail.com>
2022-06-18 14:18:17 +0600

    files added

---
+++ __steps.js
@@ -0,0 +1,25 @@
+/* 
+---------------------
+install
+---------------------
+npm i
+express js
+cors
+dotenv
+*/
+
+/* 
+    1* import express js
+    2* import cors
+    3. import dotenv
+    4* put express() to app or any constant
+    5* port = process.env.PORT || 5000
+    6* use middleware cors and express.json
+    7. app.get(), --> get() takes two parameters
+    8. inside get function make a route('/') as first parameter
+    9. second parameter takes (req, res){
+        res.send('-----')
+    }
+    10
+
+*/
---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+// middleware
+app.use(cors());
+app.use(express.json());
+
+/* 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.isfsk8s.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+*/
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.isfsk8s.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("geniusCar").collection("service");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("genius server");
+});
+
+app.listen(port, () => {
+  console.log(`listening at ${port}`);
+});
