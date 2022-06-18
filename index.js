0b2e718a4bd2d018b0f7ad96e0cbc43c477d0c23
johirhaquedipok <johirhaquedipok2@gmail.com>
2022-06-18 16:31:25 +0600

    delete updated

--- index.js
+++ index.js
@@ -52,6 +52,14 @@ async function run() {
       const result = await serviceCollection.insertOne(newService);
       res.send(result);
     });
+
+    // Delete from  data in the mongo server
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
